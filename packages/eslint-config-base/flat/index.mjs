5b6b7e87b157213fbe693d0cba2e77f3f05b23e1
Paul TGnkin <paultagankin@icloud.com>
2024-12-24 14:15:05 +0300

    refactored code and added new flat config for eslint

---
+++ packages/eslint-config-base/flat/index.mjs
@@ -0,0 +1,86 @@
+"use strict"
+
+import eslintRecommended from '@eslint/js';
+import tsEslintRecommended from '@typescript-eslint/eslint-plugin';
+import tsEslintParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+    {
+        files: ['*.ts', '*.tsx'],
+        languageOptions: {
+            parser: tsEslintParser,
+        },
+        plugins: {
+            '@typescript-eslint': tsEslintRecommended,
+            import: importPlugin,
+        },
+        rules: {
+            ...eslintRecommended.configs.recommended.rules,
+            ...tsEslintRecommended.configs['eslint-recommended'].rules,
+            ...tsEslintRecommended.configs.recommended.rules,
+            ...importPlugin.configs.recommended.rules,
+
+            '@typescript-eslint/no-empty-function': 'off',
+            'no-restricted-syntax': 'off', // В for...of циклах ничего плохого нет
+            'spaced-comment': ['error', 'always', { markers: ['/'] }], // разрешаем ts-require directive
+            'comma-dangle': ['error', 'always-multiline'],
+            'arrow-parens': ['error', 'always'],
+
+            'space-before-function-paren': [
+                'error',
+                {
+                    anonymous: 'never',
+                    named: 'never',
+                    asyncArrow: 'always',
+                },
+            ],
+            indent: 'off',
+            'max-len': [
+                'error',
+                120,
+                2,
+                {
+                    ignoreUrls: true,
+                    ignoreComments: false,
+                    ignoreRegExpLiterals: true,
+                    ignoreStrings: true,
+                    ignoreTemplateLiterals: true,
+                },
+            ],
+            'padding-line-between-statements': [
+                'error',
+                { blankLine: 'always', prev: '*', next: 'return' },
+                { blankLine: 'always', prev: '*', next: 'if' },
+            ],
+            'implicit-arrow-linebreak': 'off',
+            'no-plusplus': 'off',
+            'max-classes-per-file': 'off',
+            'operator-linebreak': 'off',
+            'object-curly-newline': 'off',
+            'class-methods-use-this': 'off',
+            'no-confusing-arrow': 'off',
+            'function-paren-newline': 'off',
+            'no-param-reassign': 'off',
+            'no-shadow': 'warn',
+            'consistent-return': 'off',
+
+            '@typescript-eslint/explicit-function-return-type': 'off',
+
+            'import/prefer-default-export': 'off', // @grape: https://humanwhocodes.com/blog/2019/01/stop-using-default-exports-javascript-module/
+            'import/order': [
+                'error',
+                {
+                    groups: [['builtin', 'external'], 'internal', 'parent', 'sibling', 'index'],
+                    'newlines-between': 'always',
+                },
+            ],
+            'import/no-unresolved': 'off',
+            'import/extensions': 'off',
+            'import/no-extraneous-dependencies': ['off'],
+            'arrow-body-style': 'off',
+            'no-unused-expressions': 'off',
+        },
+    },
+];
--- packages/eslint-config/index.mjs
+++ packages/eslint-config/index.mjs
@@ -1,90 +1,78 @@
-"use strict"
+import prettierConfig from "@salutejs/prettier-config"
 
-import eslintConfigBase from '@salutejs/eslint-config-base';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
-import reactPerfPlugin from 'eslint-plugin-react-perf';
-import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
-import saluteRulesPlugin from 'eslint-plugin-salute-rules';
-import prettierPlugin from 'eslint-plugin-prettier';
-import prettierConfig from '@salutejs/prettier-config';
+export default {
+    extends: [
+        '@salutejs/eslint-config-base',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:react-perf/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:salute-rules/all',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        'react/prop-types': 'off',
+        'react/static-property-placement': 'off',
+        'react/state-in-constructor': 'off',
+        'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+        'react/jsx-one-expression-per-line': 'off',
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'react/display-name': 'error',
+        'react/jsx-no-leaked-render': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'react/destructuring-assignment': 'off',
+        'react/sort-comp': 'off',
+        'react/no-array-index-key': 'off',
+        'react/require-default-props': 'off',
+        'react/function-component-definition': 'off',
+        'react/jsx-no-useless-fragment': 'off',
+        'react/no-unstable-nested-components': 'error',
 
-/** @type {import("eslint").Linter.Config[]} */
-export default [
-    eslintConfigBase,
-    reactPlugin.configs.recommended,
-    reactHooksPlugin.configs.recommended,
-    reactPerfPlugin.configs.recommended,
-    jsxA11yPlugin.configs.recommended,
-    saluteRulesPlugin.configs.all,
-    prettierPlugin.configs.recommended,
-    {
-        rules: {
-            'react/prop-types': 'off',
-            'react/static-property-placement': 'off',
-            'react/state-in-constructor': 'off',
-            'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
-            'react/jsx-one-expression-per-line': 'off',
-            'react/jsx-indent': ['error', 4],
-            'react/jsx-indent-props': ['error', 4],
-            'react/display-name': 'error',
-            'react/jsx-no-leaked-render': 'off',
-            'react/jsx-props-no-spreading': 'off',
-            'react/destructuring-assignment': 'off',
-            'react/sort-comp': 'off',
-            'react/no-array-index-key': 'off',
-            'react/require-default-props': 'off',
-            'react/function-component-definition': 'off',
-            'react/jsx-no-useless-fragment': 'off',
-            'react/no-unstable-nested-components': 'error',
+        'jsx-a11y/no-static-element-interactions': 'off',
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'jsx-a11y/no-noninteractive-tabindex': 'off',
 
-            'jsx-a11y/no-static-element-interactions': 'off',
-            'jsx-a11y/click-events-have-key-events': 'off',
-            'jsx-a11y/no-noninteractive-tabindex': 'off',
-
-            'no-restricted-imports': [
-                'error',
-                {
-                    patterns: [
-                        '@salutejs/plasma-ui/*',
-                        '@salutejs/plasma-tokens/*',
-                        '@salutejs/plasma-icons/*',
-                        '@salutejs/plasma-web/*',
-                        '@salutejs/plasma-b2c/*',
-                    ],
-                },
-            ],
-
-            'default-param-last': 'warn',
-
-            '@typescript-eslint/member-ordering': [
-                'warn',
-                {
-                    default: {
-                        optionalityOrder: 'required-first',
-                    },
-                },
-            ],
-            'prettier/prettier': ['error', prettierConfig],
-        },
-        overrides: [
+        'no-restricted-imports': [
+            'error',
             {
-                files: ['*.tsx?'],
-                languageOptions: {
-                    globals: {
-                        window: true,
-                        document: true,
-                    },
-                },
-                rules: {
-                    'react/no-unknown-property': 'error',
+                patterns: [
+                    '@salutejs/plasma-ui/*',
+                    '@salutejs/plasma-tokens/*',
+                    '@salutejs/plasma-icons/*',
+                    '@salutejs/plasma-web/*',
+                    '@salutejs/plasma-b2c/*',
+                ],
+            },
+        ],
+
+        'default-param-last': 'warn',
+
+        '@typescript-eslint/member-ordering': [
+            'warn',
+            {
+                default: {
+                    optionalityOrder: 'required-first',
                 },
             },
         ],
-        settings: {
-            react: {
-                version: '18.3.1',
+        'prettier/prettier': ['error', prettierConfig],
+    },
+    overrides: [
+        {
+            files: ['*.tsx?'],
+            env: {
+                browser: true,
+            },
+            globals: {
+                window: true,
+                document: true,
             },
         },
+    ],
+    settings: {
+        react: {
+            version: '18.3.1',
+        },
     },
-];
+};
